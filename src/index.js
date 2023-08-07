aa2237bee5959d487e33a50f51351b0c59362b77
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-08-07 17:24:44 +0300

    fix: add reverse cycle

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@
 module.exports = function towelSort (matrix) {
   let result = [];
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      result.push(matrix[i][j]);
+    if (i % 2 == 0) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        result.push(matrix[i][j]);
+      }
+    }
+    if (i % 2 != 0) {
+      for (let j = matrix[i].length - 1; j > 0 ; j--) {
+        result.push(matrix[i][j]);
+      }
     }
   }
   return result;
