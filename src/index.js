ffe512bbb99a66b1e44b4a6a06a894a7c321587e
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-08-07 17:57:22 +0300

    fix: add if

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
 
 module.exports = function towelSort (matrix) {
   let result = [];
-  for (let i = 0; i < matrix.length; i++) {
+  for (let i = 0;i < matrix.length; i++) {
     if (i % 2 == 0) {
       for (let j = 0; j < matrix[i].length; j++) {
         result.push(matrix[i][j]);
       }
-    } else {
-      for (let j = matrix[i].length - 1; j >= 0 ; j--) {
+    }
+    if (i % 2 != 0) {
+      for (let j = matrix[i].length - 1; j >= 0; j--) {
         result.push(matrix[i][j]);
       }
     }
