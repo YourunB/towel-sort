af7dcb44ebfb28e5c7dbec13cb8147d6b337e852
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-08-07 17:17:27 +0300

    feat: a loop is made inside the loop to iterate through the matrix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      result.push(matrix[i][j]);
+    }
+  }
+  return result;
 }
